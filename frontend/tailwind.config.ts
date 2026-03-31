f69329ba44c2bc213508be1d9ddda16254735b74
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2026-03-31 09:45:43 +0800

    feat: add tailwind

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -11,7 +11,30 @@ const config: Config = {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        slate: {
+          850: '#1e293b',
+          750: '#334155',
+        }
       },
+      animation: {
+        'in': 'animate-in',
+        'fade-in': 'fade-in',
+        'zoom-in': 'zoom-in',
+      },
+      keyframes: {
+        'animate-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        'zoom-in': {
+          '0%': { transform: 'scale(0.95)' },
+          '100%': { transform: 'scale(1)' },
+        },
+      }
     },
   },
   plugins: [],
